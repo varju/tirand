5dbc1d3c4d691fb525260617bcc72499cc49a402
Alex Varju <alex@varju.ca>
2011-05-22 13:01:53 -0700

    initial version

---
+++ Classes/CaVarjuTirandModule.m
@@ -0,0 +1,107 @@
+/**
+ * Copyright 2011 Alex Varju
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * Appcelerator Titanium is Copyright (c) 2009-2010 by Appcelerator, Inc.
+ * and licensed under the Apache Public License (version 2)
+ */
+#import "CaVarjuTirandModule.h"
+#import "TiBase.h"
+#import "TiHost.h"
+#import "TiUtils.h"
+
+@implementation CaVarjuTirandModule
+
+#pragma mark Internal
+
+// this is generated for your module, please do not change it
+-(id)moduleGUID
+{
+	return @"1b22b90c-11bc-445f-b33f-650885f8436a";
+}
+
+// this is generated for your module, please do not change it
+-(NSString*)moduleId
+{
+	return @"ca.varju.tirand";
+}
+
+#pragma mark Lifecycle
+
+-(void)startup
+{
+	// this method is called when the module is first loaded
+	// you *must* call the superclass
+	[super startup];
+	
+	NSLog(@"[INFO] %@ loaded",self);
+}
+
+-(void)shutdown:(id)sender
+{
+	// this method is called when the module is being unloaded
+	// typically this is during shutdown. make sure you don't do too
+	// much processing here or the app will be quit forceably
+	
+	// you *must* call the superclass
+	[super shutdown:sender];
+}
+
+#pragma mark Cleanup 
+
+-(void)dealloc
+{
+	// release any resources that have been retained by the module
+	[super dealloc];
+}
+
+#pragma mark Internal Memory Management
+
+-(void)didReceiveMemoryWarning:(NSNotification*)notification
+{
+	// optionally release any resources that can be dynamically
+	// reloaded once memory is available - such as caches
+	[super didReceiveMemoryWarning:notification];
+}
+
+#pragma mark Listener Notifications
+
+-(void)_listenerAdded:(NSString *)type count:(int)count
+{
+	if (count == 1 && [type isEqualToString:@"my_event"])
+	{
+		// the first (of potentially many) listener is being added 
+		// for event named 'my_event'
+	}
+}
+
+-(void)_listenerRemoved:(NSString *)type count:(int)count
+{
+	if (count == 0 && [type isEqualToString:@"my_event"])
+	{
+		// the last listener called for event named 'my_event' has
+		// been removed, we can optionally clean up any resources
+		// since no body is listening at this point for that event
+	}
+}
+
+#pragma Public APIs
+
+-(id)random:(id)args
+{
+  return [NSNumber numberWithUnsignedInt:arc4random()];
+}
+
+@end
